3a9b46417d48a26e8ec3f6c61a593b4dbc6cb7ce
shyamgithub <shyamsirodariya100@gmail.com>
2024-10-04 17:46:40 +0530

    created basic structure

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-import express from "express"
-import  {socket}  from "socket.io"
-import http from "http"
-import { Chess } from "chess.js"
+const express = require("express")
+const socket = require("socket.io")
+const http = require("http")
+const {Chess} = require("chess.js")
+const path = require("path")
+
 
 const app = express();
 const server = http.createServer(app);
 
-const io = socket(server);
+const io =   socket(server);
 const chess = new Chess();
 
+let player = {};
+let currentPlayer = "W";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname , "public")))
+
+app.get("/" , (req,res)=>{
+    res.render("index" , {title:"Chess game"})
+})
+
+io.on("connection" , function(uniqueSocket){
+    console.log("connected")
+})
+
+server.listen(3000 , function(){
+    console.log("listening on port 3000")
+})
 
